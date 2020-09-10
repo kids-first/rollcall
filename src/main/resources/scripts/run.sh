7e34d3eba24abe401f506145c649db09ec8d8b9a
jecos <jcostanza@ferlab.bio>
2020-09-10 16:53:56 -0400

    fix : kids-first/kf-portal-ui#2692 pass scheme in environment variable (2)

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -5,6 +5,7 @@ java -jar $ROLLCALL_INSTALL_PATH/install/ROLLCALL.jar \
         --server.port=$ROLLCALL_SERVER_PORT \
         --elasticsearch.host=$ROLLCALL_ES_HOST \
         --elasticsearch.port=$ROLLCALL_ES_PORT \
+        --elasticsearch.scheme=$ROLLCALL_ES_SCHEME \
         --elasticsearch.cluster-name=$ROLLCALL_ES_CLUSTER \
         --auth0.issuer=$AUTH0_ISSUER \
         --auth0.audience=$AUTH0_AUDIENCE
